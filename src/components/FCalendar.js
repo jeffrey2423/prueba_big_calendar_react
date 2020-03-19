70ecb7a9eef20298ec7bdb273cacdf0cf6babee0
Jeffrey Rios Muñoz <jeffrey.rios@siesa.com>
2020-03-18 21:31:44 -0500

    modal para consulta de evento terminada

--- src/components/FCalendar.js
+++ src/components/FCalendar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import events from '../events'
 import * as BigCalendar from 'react-big-calendar-like-google';
 import 'react-big-calendar-like-google/lib/css/react-big-calendar.css'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
 
 
 //import BigCalendar from 'react-big-calendar';
@@ -18,16 +20,36 @@ export class FCalendar extends Component {
 
                 <BigCalendar
                     selectable
+                    popup
                     events={events}
                     defaultView='week'
                     scrollToTime={new Date(1970, 1, 1, 6)}
                     defaultDate={new Date(2015, 3, 17)}
-                    onSelectEvent={event => alert(event.desc)}
-                    // onSelectSlot={(slotInfo) => alert(
-                    //     `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
-                    //     `\nend: ${slotInfo.end.toLocaleString()}` +
-                    //     `\naction: ${slotInfo.action}`
-                    // )}
+                    onSelectEvent={event =>
+
+                          Swal.fire({
+                            title: event.title,
+                            text: event.desc,
+                            showConfirmButton: false,
+                            showCloseButton: true,
+                            width: 600,
+                            padding: '3em',
+                            background: '#fff url(/images/trees.png)',
+                            backdrop: `
+                            rgba(121, 129, 134, 0.2)
+                              url("/images/nyan-cat.gif")
+                              left top
+                              no-repeat
+                            `
+                          })
+                          
+                        
+                    }
+                onSelectSlot={(slotInfo) => alert(
+                    `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
+                    `\nend: ${slotInfo.end.toLocaleString()}` +
+                    `\naction: ${slotInfo.action}`
+                )}
                 />
             </div>
         )
